6dd65ed8845273e75829f325891e3ae1ff2a9b9a
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-27 13:29:01 -0400

    Update lexer.ts

--- lexer.ts
+++ lexer.ts
@@ -4,3 +4,30 @@ export interface GrammarStruct {
   id: string;
   match: string;
 }
+
+export default class Lexer {
+  private index: number = 0;
+  private expr: string = "";
+  private regex?: RegExp;
+  public tokens: Token[] = [];
+  public column: number = 1;
+  public line: number = 1;
+  public data: string = "";
+  public grammar: GrammarStruct[] = {
+    id: "newLine",
+    match: "\\n" 
+  },
+  {
+    id: "whitespace",
+    match: "\\s"
+  }
+];
+private getRegex(){}
+public loadDefinition(def: GrammarStruct) {}
+public loadGrammar(grammar: GrammarStruct[])
+public loadData(data: string) {}
+public next(){}
+public processAll(){}
+public update(){}
+public empty(){}
+}
